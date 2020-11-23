51684004692b0f2441c79b6adab629eef9ccde16
Chris Marshall <chris@codegoalie.com>
2020-11-23 10:13:54 -0500

    Test view logic

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/codegoalie/bubbletea-test
 
 go 1.15
 
-require github.com/charmbracelet/bubbletea v0.12.2 // indirect
+require (
+	github.com/charmbracelet/bubbletea v0.12.2
+	github.com/stretchr/testify v1.6.1
+)
---
+++ view_test.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTitle(t *testing.T) {
+	m := model{}
+	expected := "Station list"
+	actual := m.View()
+
+	assert.Contains(t, actual, expected)
+}
+
+func TestListStationNames(t *testing.T) {
+	m := model{
+		choices: Stations{
+			staticStation{name: "Station0"},
+			staticStation{name: "Station1"},
+		},
+	}
+	actual := m.View()
+
+	for i, choice := range m.choices {
+		t.Run("Listing station"+strconv.Itoa(i), func(it *testing.T) {
+			assert.Contains(it, actual, choice.Name())
+		})
+	}
+}
+
+func TestShowDuration(t *testing.T) {
+	m := model{
+		choices: Stations{
+			staticStation{
+				name:     "Test",
+				duration: time.Minute,
+			},
+		},
+	}
+
+	actual := m.View()
+
+	assert.Contains(t, actual, "/ 01:00)")
+}
+
+func TestShowRemaining(t *testing.T) {
+	m := model{
+		choices: Stations{
+			staticStation{
+				name:     "Test",
+				duration: time.Minute,
+				endsAt:   time.Now().Add(3 * time.Second),
+			},
+		},
+		lastTick: time.Now(),
+	}
+
+	actual := m.View()
+
+	assert.Contains(t, actual, "(00:03 /")
+}
