cc5516a1287aa83d410e5c9cc1cd2c588ef8cc03
Chris Marshall <chris@codegoalie.com>
2022-09-07 09:33:13 -0400

    Fix Sorcer radio main stream history parsing

--- sorcer/mainStream.go
+++ sorcer/mainStream.go
@@ -7,7 +7,8 @@ import (
 )
 
 const (
-	mainInfoURL = "https://api.live365.com/station/a89268"
+	// mainInfoURL = "https://api.live365.com/station/a89268"
+	mainInfoURL = "https://listen.samcloud.com/webapi/station/67046/history?token=4e2d422c81d81eff066a193572925fa52962dd32&top=6&mediaTypeCodes=MUS,COM,NWS,INT&format=json&_=1662557064462"
 )
 
 type Main struct{}
@@ -17,14 +18,15 @@ func (m Main) Name() string {
 }
 
 func (m Main) StreamURL() string {
-	return "https://streaming.live365.com/a89268"
+	// return "https://streaming.live365.com/a89268"
+	return "https://samcloud.spacial.com/api/listen?sid=67046&rid=155533&f=mp3,any&br=128000,any&m=sc&t=ssl"
 }
 
 func (m Main) RegisterForUpdates(updates chan models.TrackUpdate) tea.Cmd {
 	return utils.SetupUpdateRegister(
 		m.Name(),
 		mainInfoURL,
-		parseLive365TrackInfo,
+		parseTrackInfo,
 		updates,
 	)
 }
