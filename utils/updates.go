80179f7bbed611e1716b080792b620025f0be3d0
Chris Marshall <chris@codegoalie.com>
2026-02-16 10:37:10 -0500

    Be smarter about when to update UI

--- utils/updates.go
+++ utils/updates.go
@@ -15,29 +15,28 @@ func SetupUpdateRegister(stationName, infoURL string, parseTrackInfo trackInfoPa
 		ticks := 0
 		ticker := time.NewTicker(time.Second)
 
-		for {
-			select {
-			case <-ticker.C:
-				ticks++
-				// TODO: Be smarter about when to update (like no more remaining)
-				if ticks%5 == 0 {
-					update := models.TrackUpdate{
-						StationName: stationName,
-					}
-					raw, err := HTTPGet(infoURL)
-					if err != nil {
-						update.Error = fmt.Errorf("failed to get atmospheres info: %w", err)
-					} else {
-						update.Info, err = parseTrackInfo(raw.Bytes())
-						if err != nil {
-							update.Error = fmt.Errorf("failed to parse atmospheres info: %w", err)
-						}
-					}
-
-					updates <- update
+		for range ticker.C {
+			ticks++
+			// TODO: Be smarter about when to update (like no more remaining)
+			if ticks%5 == 0 {
+				update := models.TrackUpdate{
+					StationName: stationName,
 				}
+				raw, err := HTTPGet(infoURL)
+				if err != nil {
+					update.Error = fmt.Errorf("failed to get atmospheres info: %w", err)
+				} else {
+					update.Info, err = parseTrackInfo(raw.Bytes())
+					if err != nil {
+						update.Error = fmt.Errorf("failed to parse atmospheres info: %w", err)
+					}
+				}
+
+				updates <- update
 			}
-			// Probably need to handle quitting or other cases here
 		}
+
+		// Probably need to handle quitting or other cases here
+		return nil
 	}
 }
